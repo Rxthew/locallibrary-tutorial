8cca7b10a8e2b3fc776c33b12b509148e635111c
rxthew <richardtheumaster@gmail.com>
2022-10-10 23:23:31 +0200

    Reformat DOB and DOD

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const {DateTime} = require("luxon")
 const AuthorSchema = new Schema({
     first_name : {type: String, required: true, maxLength: 100},
     family_name : {type: String, required: true, maxLength: 100},
@@ -20,6 +21,14 @@ AuthorSchema.virtual("name").get(function(){
 
 AuthorSchema.virtual("url").get(function(){
     return `catalog/author/${this._id}`
+})
+
+AuthorSchema.virtual("dob").get(function(){
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+})
+
+AuthorSchema.virtual("dod").get(function(){
+    return DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
 })
 
 module.exports = mongoose.model("Author", AuthorSchema)
