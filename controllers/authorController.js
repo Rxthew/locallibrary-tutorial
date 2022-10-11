23c1df2449c9e3c910a180b464c8c8d13370d778
rxthew <richardtheumaster@gmail.com>
2022-10-11 14:35:56 +0200

    Implement author detail controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -14,8 +14,35 @@ exports.author_list = (req,res,next) => {
     })
 }
 
-exports.author_detail = (req,res) => {
-    res.send(`Not Implemented: Author detail: ${req.params.id}`);
+exports.author_detail = (req,res,next) => {
+    async.parallel({
+        author: function(callback){
+            Author.findById(req.params.id)
+            .exec(callback)
+
+
+        },
+        author_books: function(callback){
+            Book.find({author: req.params.id},'title summary')
+            .exec(callback)
+
+        }
+    },function(err,results){
+        if(err){
+            return next(err)
+        }
+        if(results.author === null){
+            const genErr = new Error('Author not found')
+            genErr.status = 404
+            return next(genErr)
+        }
+        res.render('author_detail',{
+            title: 'Author Detail',
+            author: results.author,
+            author_books: results.authors_books
+        })
+
+    })
 }
 
 exports.author_create_get = (req,res) => {
